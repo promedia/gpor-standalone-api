47dda20ff7106537c8985e5f59c1651335caec37
Aleksandr Shcherbakov <a.shcherbakov@properm.ru>
2013-02-19 11:57:47 +0600

    Yii debug on

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require_once(dirname(__FILE__).'/../../yii/framework/yii.php');
 
 // debug
-defined('YII_DEBUG') or define('YII_DEBUG',false);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 $config = require(dirname(__FILE__) . '/protected/config/main.php');
 // create a Web application instance and run
