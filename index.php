a2b79496e9d780be60b0b080242770e770ad4cf0
Aleksandr Shcherbakov <a.shcherbakov@properm.ru>
2013-02-19 11:57:01 +0600

    Yii debug on

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 // include Yii bootstrap file
 require_once(dirname(__FILE__).'/../../yii/framework/yii.php');
 
+// debug
+defined('YII_DEBUG') or define('YII_DEBUG',false);
+
 $config = require(dirname(__FILE__) . '/protected/config/main.php');
 // create a Web application instance and run
 Yii::createWebApplication($config)->run();
