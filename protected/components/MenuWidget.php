d8bfaa64b35d401c09e91eb203ec5de219e5bda4
rusakova <l.rusakova@properm.ru>
2013-03-30 16:01:54 +0600

    APIPP-15

--- protected/components/MenuWidget.php
+++ protected/components/MenuWidget.php
@@ -7,55 +7,54 @@
  */
 class MenuWidget extends CWidget {
 
-  public $legacy = '';
-  public $authUser = '';
-  public $activeItemId = 0;
+    public $legacy = '';
+    public $authUser = '';
+    public $activeItemId = 0;
 
-  public function run() {
+    public function run() {
 
-    // get page url
-    $pageUri = Yii::app()->request->requestUri;
-    preg_match('/url=(.+?)\&/', $pageUri, $matches);
-    $pageUrl = $matches[1];
+        // get page url
+        $pageUrl = Yii::app()->request->getQuery('url');
 
-    // get rules for checked menu items from 66 api
-    $arrRules = XMLRPCHelper::sendMessage('admin.listMenuCheckedRules');
+        // get rules for checked menu items from 66 api
+        $arrRules = XMLRPCHelper::sendMessage('admin.listMenuCheckedRules');
 
-    // get active item of menu
-    foreach ($arrRules as $rule) {
-      $mask = preg_replace('/\//', '', $rule['rule']);
+        // get active item of menu
+        foreach ($arrRules as $rule) {
+            $mask = addcslashes($rule['rule'], '/');
+            $mask = preg_replace('/\*/', '(.*)', $mask);
 
-      if (preg_match('/^' . $pageUrl . '/', $mask)) {
-        $activeItemId = $rule['siteMenuItemId'];
-        break;
-      }
+            if (preg_match('/^' . $mask . '$/', $pageUrl)) {
+                $activeItemId = $rule['siteMenuItemId'];
+                break;
+            }
+        }
+
+        // get cache
+        $cacheKey = md5(serialize(array('type' => 'menu', 'activeItem' => $activeItemId, 'authUser' => $this->authUser)));
+
+        $widgetCache = Yii::app()->cache->get($cacheKey);
+
+        // if isset cache return it
+        if ($widgetCache) {
+
+            $this->render('MenuView' . $this->legacy, array('data' => $widgetCache));
+        } else {
+
+            $cacheTime = !empty(Yii::app()->params['cachingPeriod']['menu']) ? Yii::app()->params['cachingPeriod']['menu'] : 24 * 60 * 60;
+
+            // get rules for checked menu items from 66 api
+            $arrMenuData = XMLRPCHelper::sendMessage('admin.listMenu');
+
+            if (is_array($arrMenuData)) {
+
+                // save cache
+                Yii::app()->cache->set($cacheKey, $arrMenuData, $cacheTime);
+
+                $this->render('MenuView' . $this->legacy, array('data' => $arrMenuData, 'activeItem' => $activeItemId));
+            }
+        }
     }
-
-    // get cache
-    $cacheKey = md5(serialize(array('type' => 'menu', 'activeItem' => $activeItemId, 'authUser' => $this->authUser)));
-
-    $widgetCache = Yii::app()->cache->get($cacheKey);
-
-    // if isset cache return it
-    if ($widgetCache) {
-
-      $this->render('MenuView' . $this->legacy, array('data' => $widgetCache));
-    } else {
-
-      $cacheTime = !empty(Yii::app()->params['cachingPeriod']['menu']) ? Yii::app()->params['cachingPeriod']['menu'] : 24 * 60 * 60;
-
-      // get rules for checked menu items from 66 api
-      $arrMenuData = XMLRPCHelper::sendMessage('admin.listMenu');
-
-      if (is_array($arrMenuData)) {
-
-        // save cache
-        Yii::app()->cache->set($cacheKey, $arrMenuData, $cacheTime);
-
-        $this->render('MenuView' . $this->legacy, array('data' => $arrMenuData, 'activeItem' => $activeItemId));
-      }
-    }
-  }
 
 }
 
