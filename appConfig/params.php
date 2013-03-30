21c39f4b760ee1d0f2401489ec84e4d4df8a00c5
e-shiryaev <e.shiryaev@properm.ru>
2013-03-30 18:26:41 +0600

    merge menu

--- appConfig/params.php
+++ appConfig/params.php
@@ -6,14 +6,16 @@ $params = array(
         'host' => 'api.newperm.gpor.ru',
         'port' => '80',
         'path' => '/',
-        'key' => ''
+        'key' => 'cfe2b2498aec5b086ac9da1a67bd2fe9'
     ),
     'cachingPeriod' => array(
         'CB' => array(
             'default' => 60 * 60,
             'common_banner_top' => 24 * 60 * 60,
         ),
-        'header' => 60 * 60,
+        'header' => 1,
+        'weatherInformer' => 60*60,
+        'menu' => 1
     ),
     'token' => array(
         'default'
