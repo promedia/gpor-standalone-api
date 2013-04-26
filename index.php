cd1b7d1e0384c5c95ef5ca665f54b39c4af80baf
korepanova <d.korepanova@properm.ru>
2013-04-26 11:16:10 +0600

    Set off debug

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 // debug
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 // include Yii bootstrap file
-require_once(dirname(__FILE__).'/../../yii/framework/yii.php');
+require_once(dirname(__FILE__).'/../yii/framework/yii.php');
 
 $config = require(dirname(__FILE__) . '/protected/config/main.php');
 // create a Web application instance and run
