056a7fe4d73296eabecaf56de4d5f1a4b1959140
korepanova <d.korepanova@properm.ru>
2013-04-26 11:08:58 +0600

    Update default cahce params

--- appConfig/params.php
+++ appConfig/params.php
@@ -3,19 +3,20 @@
 // Application configuration.
 $params = array(
     'api' => array(
-        'host' => 'api.newperm.gpor.ru',
+        'host' => 'api.properm.ru',
         'port' => '80',
         'path' => '/',
         'key' => ''
     ),
     'cachingPeriod' => array(
+        'listCustomUrlTitles' => 24 * 60 * 60,
         'CB' => array(
             'default' => 60 * 60,
             'common_banner_top' => 24 * 60 * 60,
         ),
-        'header' => 60 * 60,
-        'weatherInformer' => 60*60,
-        'menu' => 24 * 60 * 60
+        'menu' => 24 * 60 * 60,
+        'currencyInformer' => 24 * 60 *60,
+        'weatherInformer' => 60*60        
     ),
     'token' => array(
         'default'
