787945319b81d986357bbd2b64c14bdebc3c2ee3
l.rusakova <l.rusakova@properm.ru>
2013-06-07 11:00:58 +0600

    APIPP-7
    rendering menu for athorized users

--- protected/components/views/MenuViewLegacy.php
+++ protected/components/views/MenuViewLegacy.php
@@ -1,50 +1,82 @@
 <ul class="v5-menu">
 
-  <?php
-  foreach ($data as $id => $item) {
+    <?php
+    foreach ($data as $id => $item) {
 
-    if ($item['status'] == 10) {
+        if ($item['status'] == 10) {
 
-      if ($item['id'] == $activeItem) {
-        ?>
-        <li class="v5-menu-item current">
+            if ($item['id'] == $activeItem) {
+                ?>
+                <li class="v5-menu-item current">
 
-        <?php } else { ?>
-        <li class="v5-menu-item">
-
-        <?php } ?>
-
-        <a href="<?= $item['url'] ?>"><?= $item['label'] ?></a>
-
-        <?php
-        if (!empty($item['items'])) {
-          ?>
-
-          <div class="v5-submenu-wrap">
-            <div class="v5-submenu"><div class="i-hate-opera">&nbsp;</div>
-              <ul class="v5-submenu">
-
-                <?php
-                foreach ($item['items'] as $id => $subitem) {
-                  ?>
-
-                  <li class="v5-submenu-item">
-                    <a href="<?= $subitem['url'] ?>" ><?= $subitem['label'] ?></a>
-                  </li>
+                <?php } else { ?>
+                <li class="v5-menu-item">
 
                 <?php } ?>
 
-              </ul>
-            </div>
-          </div>
+                <a href="<?= $item['url'] ?>"><?= $item['label'] ?></a>
 
-        <?php } ?>
+                <?php
+                if (!empty($item['items'])) {
+                    ?>
 
-      </li>
+                    <div class="v5-submenu-wrap">
+                        <div class="v5-submenu"><div class="i-hate-opera">&nbsp;</div>
+                            <ul class="v5-submenu">
 
-      <?php
+                                <?php
+                                foreach ($item['items'] as $id => $subitem) {
+                                    ?>
+
+                                    <li class="v5-submenu-item">
+                                        <a href="<?= $subitem['url'] ?>" ><?= $subitem['label'] ?></a>
+                                    </li>
+
+                                <?php } ?>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            </li>
+
+            <?php
+        } elseif (($flag == 1) && ($item['status'] == 20)) {
+            ?>
+            <li class="v5-menu-item">
+
+                <a href="<?= $item['url'] ?>"><?= $item['label'] ?></a>
+
+                <?php
+                if (!empty($item['items'])) {
+                    ?>
+
+                    <div class="v5-submenu-wrap">
+                        <div class="v5-submenu"><div class="i-hate-opera">&nbsp;</div>
+                            <ul class="v5-submenu">
+
+                                <?php
+                                foreach ($item['items'] as $id => $subitem) {
+                                    ?>
+
+                                    <li class="v5-submenu-item">
+                                        <a href="<?= $subitem['url'] ?>" ><?= $subitem['label'] ?></a>
+                                    </li>
+
+                                <?php } ?>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            </li>
+        <?php
+        }
     }
-  }
-  ?>
+    ?>
 
 </ul>
