b47b6699f7b5f96095553cbcb7a2f4cc42b9f5d6
www <www@cuba.office.promedia-perm.ru>
2013-12-12 12:15:48 +0600

    renewal

--- appConfig/params.php
+++ appConfig/params.php
@@ -3,22 +3,28 @@
 // Application configuration.
 $params = array(
     'api' => array(
-        'host' => 'api.newperm.gpor.ru',
+        'host' => 'api.properm.ru',
         'port' => '80',
         'path' => '/',
         'key' => ''
     ),
     'cachingPeriod' => array(
+        'listCustomUrlTitles' => 24 * 60 * 60,
         'CB' => array(
             'default' => 60 * 60,
             'common_banner_top' => 24 * 60 * 60,
         ),
-        'header' => 60 * 60,
-        'weatherInformer' => 60*60,
-        'menu' => 24 * 60 * 60
+        'menu' => 24 * 60 * 60,
+        'currencyInformer' => 24 * 60 *60,
+        'weatherInformer' => 60*60        
     ),
     'token' => array(
-        'default'
+        'default'	
+    ),
+    'authData' => array(
+        'clientId' => 'demo', // логин клиента на бекэнде
+        'clientSecret' => 'demo', // пароль клиента на бекэнде
+        'authHost' => 'http://auth.properm.ru' // хост бекэнда
     )
 );
 
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 // include Yii bootstrap file
-require_once(dirname(__FILE__).'/../../yii/framework/yii.php');
+require_once(dirname(__FILE__).'/../yii/framework/yii.php');
 
 $config = require(dirname(__FILE__) . '/protected/config/main.php');
 // create a Web application instance and run
